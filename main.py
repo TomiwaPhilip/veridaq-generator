c7a86e194445b4eaa14f24f28ad65b3366493329
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-07-09 07:37:04 +0000

    added handson reference

--- main.py
+++ main.py
@@ -69,6 +69,39 @@ def getDoc2():
         return "Only GET requests with JSON data are allowed", 400
 
 
+@app.route('/handson-reference', methods=["POST"])
+def getDoc7():
+    if request.method == "POST" and request.is_json:
+        try:
+            data = request.json  # Access the JSON data from the request
+            # Extract required fields from the JSON data
+            nameOfEmployee = data["nameOfEmployee"]
+            identifier = data["identifier"]
+            roleType = data["roleType"]
+            subType = data["subType"]
+            role = data["role"]
+            projectTitle = data["projectTitle"]
+            period = data["period"]
+            jobFunctions = data["jobFunctions"]
+            nameOfAdmin = data["nameOfAdmin"]
+            badgeID = data["badgeID"]
+
+            nameOfInstitution = data.get("nameOfInstitution", "NIL")
+            adminDesignation = data.get("adminDesignation", "NIL")
+            currentDateTime = data.get("currentDateTime", "NIL")
+            notableAchievement = data.get("notableAchievement", "NIL")
+            personalitySummary = data.get("personalitySummary", "NIL")
+            
+            # Generate the PDF using the received data
+            return generateWorkReference(nameOfEmployee, identifier, roleType, nameOfInstitution, subType, 
+                                          role, projectTitle, period, jobFunctions, notableAchievement, 
+                                          personalitySummary, nameOfAdmin, adminDesignation, currentDateTime, badgeID)
+        except KeyError as e:
+            return f"Missing required field: {e}", 400
+    else:
+        return "Only GET requests with JSON data are allowed", 400
+    
+
 @app.route('/student-status', methods=["POST"])
 def getDoc3():
     if request.method == "POST" and request.is_json:
